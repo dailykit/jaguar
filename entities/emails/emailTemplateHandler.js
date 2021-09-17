46fde59628fbad7b8e5ad6c8289d4db74d5498e9
Deepak Negi <st.deepak15@gmail.com>
2021-09-17 14:56:32 +0530

    feat:payload type check on emailTemplate

--- entities/emails/emailTemplateHandler.js
+++ entities/emails/emailTemplateHandler.js
@@ -2,16 +2,20 @@ import { emailTrigger } from '../../utils'
 
 export const emailTemplateHandler = async (req, res) => {
    try {
-      const { payload = null } = req.body
-      const parsedPayload = JSON.parse(payload)
-      if (parsedPayload) {
+      let payload
+      if (typeof req.body.payload === 'string') {
+         payload = JSON.parse(req.body.payload)
+      } else {
+         payload = req.body.payload
+      }
+      if (payload) {
          const result = await emailTrigger({
-            title: parsedPayload.emailTriggerTitle,
-            variables: parsedPayload,
-            to: parsedPayload.email,
-            brandId: parsedPayload.brandId,
-            includeHeader: parsedPayload.includeHeader,
-            includeFooter: parsedPayload.includeFooter
+            title: payload.emailTriggerTitle,
+            variables: payload,
+            to: payload.email,
+            brandId: payload.brandId,
+            includeHeader: payload.includeHeader,
+            includeFooter: payload.includeFooter
          })
          res.status(result.success ? 200 : 400).json(result)
       }
